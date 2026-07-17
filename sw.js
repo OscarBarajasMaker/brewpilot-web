b87f978e9474af3cdcc868eb5a4848881389e6ce
OscarBarajasMaker <oscarjpbarajas@gmail.com>
2026-07-17 13:11:00 -0600

    Replace the Apps Script wizard with the one tap Drive step and fix the help bubbles dying on language switch

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // first meant they were only served offline, so this was survivable rather than
 // visible, which is the worst kind of bug. A new build now gets a new cache and
 // the old one is deleted on activate.
-var CACHE = 'brewpilot-2026-07-17-0241-75e284';
+var CACHE = 'brewpilot-2026-07-17-1828-73c48f';
 
 self.addEventListener('install', function (e) {
   self.skipWaiting();
