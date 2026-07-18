66a1245a6be3610d4dd06426ce23cc2cf9514ef9
OscarBarajasMaker <oscarjpbarajas@gmail.com>
2026-07-18 13:16:41 -0600

    move open-sheet out of log form into Settings google card as a bordered button, remove form sheet link

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // first meant they were only served offline, so this was survivable rather than
 // visible, which is the worst kind of bug. A new build now gets a new cache and
 // the old one is deleted on activate.
-var CACHE = 'brewpilot-2026-07-17-2135-296db4';
+var CACHE = 'brewpilot-2026-07-18-1827-3d8ee2';
 
 self.addEventListener('install', function (e) {
   self.skipWaiting();
